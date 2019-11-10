3da57a0a989ec6b756eed4758c4c11a52616c0e7
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-10 17:22:15 -0300

    create routes user login and register

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,6 +6,8 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 
 // Load Input Validation
+const validateResgisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -13,6 +15,98 @@ const User = require('../../models/User');
 // @route GET /api/users/test
 // @desc Test users route
 // @access Public
-router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+router.get('/test', (req, res) => res.send({ msg: 'Users Works' }));
+
+// @route POST /api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', async (req, res) => {
+    const { errors, isValid } = validateResgisterInput(req.body);
+    const { name, email, password } = req.body;
+
+    // check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    // Find User
+    const user = await User.findOne({ email });
+    if (user) {
+        errors.email = 'Email já existe na base de dados';
+        return res.status(400).json(errors);
+    }
+    // New User 
+    const newUser = new User({name, email, password});
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+    });
+});
+
+// @route   POST api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', async (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+  
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  
+    const { email, password } = req.body;
+  
+    // Find user by email
+    const user = await User.findOne({ email });
+    // Check for user
+    if (!user) {
+        errors.email = 'Usuario não encontrado';
+        return res.status(404).json(errors);
+    }
+    // Check Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+            res.json({
+                success: true,
+                token: 'Bearer ' + token,
+                expiresIn: 3600,
+                localId: user.id
+            });
+        });
+    } else {
+        errors.password = 'Senha incorreta';
+        return res.status(400).json(errors);
+    }
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+      });
+    }
+);
 
 module.exports = router;

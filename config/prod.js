2c8afd57a777ae09a19e175c0ecbab7ef38685c6
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-11 00:18:59 -0300

    set keys to heroku

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secret: process.env.SECRET
+    secret: process.env.SECRET,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }

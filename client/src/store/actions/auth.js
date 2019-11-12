e1dcfad9a2b05b6f6cb17055ee4cec7731b56712
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-11 23:12:16 -0300

    made the general part of auth actions and reducer

--- client/src/store/actions/auth.js
+++ client/src/store/actions/auth.js
@@ -7,19 +7,84 @@ export const authStart = () => {
     };
 };
 
+export const authSuccess = (token, userId, manager = false) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        token: token,
+        userId: userId,
+        manager: manager
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error
+    };
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+    // tem que remover a tag de manager
+    // localStorage.removeItem('manager');
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+};
+
+export const checkAuthTimeout = (expirationTime) => dispatch => {
+    setTimeout(() => {
+        dispatch(logout());
+    }, expirationTime * 1000);
+};
+
 export const auth = (email, pass) => async dispatch => {
     dispatch(authStart());
     const authData = { email, password: pass};
     let url = `/api/users/login`;
-    const response = await axios.post(url, authData);
-    console.log(response)
-    if (response) {
-        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('expirationDate', expirationDate);
-        localStorage.setItem('userId', response.data.localId);
-        // tem que criar um local storage pra saber se o user é adim ou não
-        //tem que mudar o model da collection pra incluir essa tag
-        // localStorage.setItem('manager', response.data.manager); 
+    try {
+        const response = await axios.post(url, authData);
+        console.log(response)
+        if (response) {
+            const { token, expiresIn, localId } = response.data;
+            const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+            localStorage.setItem('token', token);
+            localStorage.setItem('expirationDate', expirationDate);
+            localStorage.setItem('userId', localId);
+            // tem que criar um local storage pra saber se o user é adim ou não
+            //tem que mudar o model da collection pra incluir essa tag
+            // localStorage.setItem('manager', response.data.manager);
+            dispatch(authSuccess(token, localId));
+            dispatch(checkAuthTimeout(expiresIn));
+        }
+    } catch (error) {
+        console.log(error.response)
+    }
+    
+}
+
+export const setAuthRedirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path
+    }
+}
+
+export const authCheckState = () => dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        dispatch(logout());
+    } else {
+        const expirationDate = new Date(localStorage.getItem('expirationDate'));
+        if (expirationDate <= new Date()) {
+            dispatch(logout());
+        } else {
+            const userId = localStorage.getItem('userId');
+            const manager = localStorage.getItem('manager');
+            dispatch(authSuccess(token, userId, manager));
+            dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+        }
     }
 }
--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -4,7 +4,7 @@ import { updateObject } from '../utility';
 const initialState = {
     token: null,
     userId: null,
-    admin: false,
+    manager: false,
     error: null,
     loading: false,
     authRedirectPath: '/'
@@ -17,9 +17,44 @@ const authStart = (state, action) => {
     });
 };
 
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.token,
+        userId: action.userId,
+        manager: action.manager,
+        error: null,
+        loading: false
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+};
+
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null,
+        manager: false
+    });
+}
+
+const setAuthRedirectPath = (state, action) => {
+    return updateObject(state, {
+        authRedirectPath: action.path
+    });
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
+        case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action);
         default: return state;
     }
 }

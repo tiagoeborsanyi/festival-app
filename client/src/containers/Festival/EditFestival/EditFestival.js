0266bb840a9b33ccd84bd594842527c7394a745d
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-12 10:20:40 -0300

    made the base part of the festival registration container

--- client/src/containers/Festival/EditFestival/EditFestival.js
+++ client/src/containers/Festival/EditFestival/EditFestival.js
@@ -3,8 +3,38 @@ import React, { Component } from 'react';
 import FormFestival from '../../../components/FormFestival/FormFestival';
 
 class EditEvent extends Component {
+    state = {
+        festival: {
+            active: false,
+            name: '',
+            phone: '',
+            mobile: '',
+            nameCompany: '',
+            state: '',
+            city: '',
+            email: '',
+            EventDate: '',
+            description: ''
+        }
+    }
+
+    changeFields = event => {
+        event.preventDefault();
+        const { value, name } = event.target;
+        const updateFestival = { ...this.state.festival };
+        updateFestival[name] = value;
+        this.setState({festival: updateFestival});
+    }
+
+    festivalSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.festival)
+    }
+
     render () {
-        return <FormFestival />
+        return <FormFestival
+                festivalChanged={this.changeFields}
+                festivalSubmit={this.festivalSubmit} />
     }
 }
 

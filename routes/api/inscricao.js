c64a55ce4deda8829e6aff61fdcd7ce08f8cc5b8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-12-05 11:19:14 -0300

    error in upload to fibase storage

--- routes/api/inscricao.js
+++ routes/api/inscricao.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { createWriteStream } = require("fs");
+const Multer = require('multer');
 const { Storage } = require("@google-cloud/storage");
 const keys = require('../../config/keys');
 
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+    }
+});
 // Load Input Validation
 
 // Load User model
@@ -30,14 +36,55 @@ router.post('/', passport.authenticate('jwt', { session: false }), async (req, r
     }
 });
 
-router.post('/upload', passport.authenticate('jwt', { session: false }), (req, res) => {
-    // console.log(req.files);
+router.post('/upload', passport.authenticate('jwt', { session: false }), multer.single('file'), (req, res) => {
+    console.log(req.files.file);
+    
+    let file = req.files.file
+    if (file) {
+        uploadImageToStorage(file).then((success) => {
+          res.status(200).send({
+            status: 'success'
+          });
+        }).catch((error) => {
+          console.error('ERRO ALGUM: ', error);
+        });
+    }
+    res.send({ok: 'file ok'})
+});
+
+const uploadImageToStorage = (file) => {
     const gc = new Storage({
         keyFilename: keys.firebase,
         projectId: 'festival-climb'
     });
-    const coolFilesBucket = gc.bucket('festival-climb');
-    res.send({ok: 'file ok'})
-})
+    const bucket = gc.bucket('festival-climb');
+    return new Promise((resolve, reject) => {
+      if (!file) {
+        reject('No image file');
+      }
+      let newFileName = `${file.name}gdfgrretyerhre${Date.now()}`;
+      console.log(newFileName)
+      let fileUpload = bucket.file(newFileName);
+  
+      const blobStream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: file.mimetype
+        }
+      });
+  
+      blobStream.on('error', (error) => {
+          console.log('STREAAAAAAM ERRRROOOOOOO', error)
+        reject('Something is wrong! Unable to upload at the moment.');
+      });
+  
+      blobStream.on('finish', () => {
+        // The public URL can be used to directly access the file via HTTP.
+        const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
+        resolve(url);
+      });
+  
+      blobStream.end(file.buffer);
+    });
+}
 
 module.exports = router;
